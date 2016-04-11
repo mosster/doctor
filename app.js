0e6b8703ab6cce657a5416543d03267b6b58962c
Ed Moss <Ed@Ed.local>
2016-04-10 23:37:06 -0400

    add styles and postcss

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 // ------------------------------
 // Module Dependencies
 // ------------------------------
-var express = require('express')
+var express = require('express');
+var PORT = 3000;
+var app = express();
 
-// ------------------------------
-// Initialize Express
-// ------------------------------
-var app = express()
-
-app.set('views', __dirname + '/app/views')
-app.set('view engine', 'jade')
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/build'));
 
 // ------------------------------
 // Routes
 // ------------------------------
 app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'Home' }
+  res.render('index', { 
+      title : 'Home' 
+    }
   )
-})
-app.listen(3000)
+});
+
+app.listen(PORT);
+console.log('Listening on port ' + PORT);
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,44 @@
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    csso        = require('gulp-csso'),
-    uglify      = require('gulp-uglify'),
-    jade        = require('gulp-jade'),
-    concat      = require('gulp-concat'),
-    livereload  = require('gulp-livereload'),
-    tinylr      = require('tiny-lr'),
-    server      = tinylr();
-    tasks       = ['js','css','templates','watch'];
- 
+var gulp         =  require('gulp'),
+    colors       =  require('gulp-util').colors,
+    concat       =  require('gulp-concat'),
+    csso         =  require('gulp-csso'),
+    jade         =  require('gulp-jade'),
+    uglify       =  require('gulp-uglify'),
+    livereload   =  require('gulp-livereload'),
+    postcss      =  require('gulp-postcss'),
+    prefix       =  require('autoprefixer'),
+    tinylr       =  require('tiny-lr'),
+    sass         =  require('gulp-sass'),
+    server       =  tinylr();
+    tasks        =  ['js','css','templates','watch'];
+
 // ---------------------------------------
 // Watch Task
 // ---------------------------------------
 gulp.task('default', tasks);
 
+console.log(colors.green('======================================================='));
+console.log(colors.green('Start tasks'));
+console.log(colors.green('======================================================='));
+
 // ---------------------------------------
 // Styling
 // ---------------------------------------
 gulp.task('css', function() {
-  return gulp.src('assets/sass/*.scss')
+  return gulp.src('assets/sass/**/*.scss')
     .pipe( 
       sass({ 
-        includePaths: ['assets/sass'],
+        includePaths: ['assets/sass/**/*'],
         errLogToConsole: true
-      } ) )
+      })
+    )
+    .pipe(
+      postcss([ 
+        prefix({ 
+          browsers: ['last 2 versions'] 
+        }) 
+      ])
+    )
     .pipe( csso() )
     .pipe( gulp.dest('build/css/') )
     .pipe( livereload( server ));
@@ -33,7 +48,7 @@ gulp.task('css', function() {
 // Scripts
 // ---------------------------------------
 gulp.task('js', function() {
-  return gulp.src('assets/js/*.js')
+  return gulp.src('assets/js/*/*.js')
     .pipe( uglify() )
     .pipe( concat('main.min.js'))
     .pipe( gulp.dest('build/js/'))
@@ -45,9 +60,11 @@ gulp.task('js', function() {
 // ---------------------------------------
 gulp.task('templates', function() {
   return gulp.src('app/views/*.jade')
-    .pipe(jade({
-      pretty: true
-    }))
+    .pipe( 
+      jade({
+        pretty: true
+      })
+    )
     .pipe(gulp.dest('build/'))
     .pipe( livereload( server ));
 });
@@ -60,8 +77,8 @@ gulp.task('watch', function () {
     if (err) {
       return console.log(err);
     }
-    gulp.watch('assets/sass/*.scss',['css']);
-    gulp.watch('assets/js/*.js',['js']);
+    gulp.watch('assets/sass/**/*.scss',['css']);
+    gulp.watch('assets/js/**/*.js',['js']);
     gulp.watch('app/views/*.jade',['templates']);
     
   });
