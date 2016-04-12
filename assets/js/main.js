01c44787a1dbae34406d45db47df8dc0ec346754
Ed Moss <Ed@Ed.local>
2016-04-12 00:41:00 -0400

    break up files and hook up smoothstate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-console.log("Goodbye World!");
+;(function($) {
+  'use strict';
+  var $body = $('html, body'),
+      content = $('.wrapper-intro').smoothState({
+        onStart: {
+          duration: 250, 
+          render: function (url, $container) {
+            content.toggleAnimationClass('is-exiting');
+            $body.animate({
+              scrollTop: 0
+            });
+          }
+        }
+      }).data('smoothState');
+})(jQuery);
