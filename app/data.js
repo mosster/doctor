535123c859ec5430e3d245da1895cb1ae52801d4
Ed Moss <Ed@Ed.local>
2016-07-11 12:57:22 -0400

    externalize content

--- app/data.js
+++ app/data.js
@@ -1,6 +1,45 @@
 module.exports = {
-    "bma": {
-        title: 'Holler',
-        clinicName: "Laskin !!!!"
-    }
+  "bma": { 
+    clinic: {
+      name: "Barnegat Medical Associates",
+      phone: "(609) 698-3636",
+      category: "Internal Medicine",
+      state: "NJ",
+      city: "Barnegat",
+      address: "218 Pocohontas Ave",
+      zip: " 08005"
+    },
+    about: "<p>Being the first of its kind, laskin introduces new age cosmetic wonders under the dermatologist's wand for skin procedures like acne scar &amp; stretch mark reduction, hyper-pigmentation, photo damaged skin, laser hair reduction, narrow band uvb therapy for psoriasis &amp; vitiligo, skin tightening and laser tattoo removal amongst other procedures.</p><p>Laskin also takes great pride in their new and unique approach to fat loss & body sculpting with low frequency ultrasound technology followed controlled volumetric heating with radio frequency that enables sculpting, lifting and toning of the face and body for the desired look you have always wanted.</p>",
+    doctors: [
+      {
+        photo: "http://placehold.it/100x100",
+        specialty: "Dermatologist",
+        name: "Dr. Rajiv Sahay",
+        desc: "MBBS , MD - Dermatology , Venereology & Leprosy, Fellowship in Cutaneous Surgeries"
+      },
+      {
+        photo: "http://placehold.it/100x100",
+        specialty: "Dermatologist",
+        name: "Dr. Nishi Sahay",
+        desc: "MBBS , MD - Dermatology , Venereology & Leprosy, Fellowship in Cutaneous Surgeries"
+      }
+    ],
+    services: [
+      "Consultation",
+      "Dermatology",
+      "Laser Hair Reduction",
+      "Acne &amp; Pigmentation Reductions",
+      "Scars &amp; Stretch Mark Removal",
+      "Face Lift &amp; Tightening",
+      "Botox &amp; Fillers",
+      "Colon Hydrotherapy",
+      "Warts",
+      "Moles &amp; Freckles Removal",
+      "Medical Peels",
+      "Mole Removal",
+      "Dermatology",
+      "Warts/Mole Removal",
+      "Acne Treatment"
+    ]
+  },
 }
