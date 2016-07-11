5e220a4007e3d5a48047df9e4c548e1ec934c173
Ed Moss <Ed@Ed.local>
2016-07-11 13:22:37 -0400

    move gallery and schedule data

--- app/data.js
+++ app/data.js
@@ -7,7 +7,12 @@ module.exports = {
       state: "NJ",
       city: "Barnegat",
       address: "218 Pocohontas Ave",
-      zip: " 08005"
+      zip: " 08005",
+      directions: "https://www.google.com/maps/dir/''/218+Pocohontas+Ave,+Barnegat,+NJ+08005/@39.773041,-74.277196,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89c1a75cdb07ffd1:0x302594c3c5b4ca65!2m2!1d-74.207156!2d39.773062",
+      schedule: {
+        days: "Monday through Saturday",
+        time: "9:00am &mdash; 5:00pm"
+      }
     },
     about: "<p>Being the first of its kind, laskin introduces new age cosmetic wonders under the dermatologist's wand for skin procedures like acne scar &amp; stretch mark reduction, hyper-pigmentation, photo damaged skin, laser hair reduction, narrow band uvb therapy for psoriasis &amp; vitiligo, skin tightening and laser tattoo removal amongst other procedures.</p><p>Laskin also takes great pride in their new and unique approach to fat loss & body sculpting with low frequency ultrasound technology followed controlled volumetric heating with radio frequency that enables sculpting, lifting and toning of the face and body for the desired look you have always wanted.</p>",
     doctors: [
@@ -23,6 +28,16 @@ module.exports = {
         name: "Dr. Nishi Sahay",
         desc: "MBBS , MD - Dermatology , Venereology & Leprosy, Fellowship in Cutaneous Surgeries"
       }
+    ],
+    gallery: [
+      "http://placehold.it/401x250",
+      "http://placehold.it/400x250",
+      "http://placehold.it/400x250",
+      "http://placehold.it/400x250",
+      "http://placehold.it/400x250",
+      "http://placehold.it/400x250",
+      "http://placehold.it/400x250",
+      "http://placehold.it/400x250"
     ],
     services: [
       "Consultation",
@@ -43,3 +58,4 @@ module.exports = {
     ]
   },
 }
+
